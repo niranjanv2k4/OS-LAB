f54791c81d66c42b3f4eba65be05450e6c9a1827
Niranjan <niranjanv2k4@gmail.com>
2025-02-18 17:12:38 +0000

    STAGE 19

---
+++ spl/spl_progs/S-19/compiler.sh
@@ -0,0 +1,45 @@
+cd $HOME/myexpos/spl
+echo "os-startup"
+./spl ./spl_progs/new/os-startup.spl
+echo "exception"
+./spl ./spl_progs/new/exception.spl
+echo "boot module"
+./spl ./spl_progs/new/boot_module.spl
+echo "timer"
+./spl ./spl_progs/new/timer.spl
+echo "disk"
+./spl ./spl_progs/new/disk.spl
+echo "console"
+./spl ./spl_progs/new/console.spl
+echo "int9"
+./spl ./spl_progs/new/int9.spl
+echo "int7"
+./spl ./spl_progs/new/int7.spl
+echo "int6"
+./spl ./spl_progs/new/int6.spl
+echo "int10"
+./spl ./spl_progs/new/int10.spl
+./spl ./spl_progs/new/haltprog.spl
+echo "mod5"
+./spl ./spl_progs/new/mod5.spl
+echo "mod4"
+./spl ./spl_progs/new/mod4.spl
+echo "mod2"
+./spl ./spl_progs/new/mod2.spl
+echo "mod1"
+./spl ./spl_progs/new/mod1.spl
+echo "mod0"
+./spl ./spl_progs/new/mod0.spl
+
+cd $HOME/myexpos/expl
+echo "idle"
+./expl ../spl/spl_progs/new/idle.expl
+echo "init"
+./expl ../spl/spl_progs/new/init.expl
+echo "linked"                               
+./expl ../spl/spl_progs/new/linked.expl
+
+cd $HOME/myexpos/xfs-interface
+./xfs-interface run ../spl/spl_progs/new/run.sh
+
+echo "COMPLETED"
---
+++ spl/spl_progs/S-19/run.sh
@@ -0,0 +1,20 @@
+fdisk
+load --os $HOME/myexpos/spl/spl_progs/new/os-startup.xsm
+load --module 7 $HOME/myexpos/spl/spl_progs/new/boot_module.xsm
+load --module 5 $HOME/myexpos/spl/spl_progs/new/mod5.xsm
+load --module 4 $HOME/myexpos/spl/spl_progs/new/mod4.xsm
+load --module 2 $HOME/myexpos/spl/spl_progs/new/mod2.xsm
+load --module 1 $HOME/myexpos/spl/spl_progs/new/mod1.xsm
+load --module 0 $HOME/myexpos/spl/spl_progs/new/mod0.xsm
+load --int=timer $HOME/myexpos/spl/spl_progs/new/timer.xsm
+load --int=disk $HOME/myexpos/spl/spl_progs/new/disk.xsm
+load --int=console $HOME/myexpos/spl/spl_progs/new/console.xsm
+load --int=9 $HOME/myexpos/spl/spl_progs/new/int9.xsm
+load --int=7 $HOME/myexpos/spl/spl_progs/new/int7.xsm
+load --int=6 $HOME/myexpos/spl/spl_progs/new/int6.xsm
+load --int=10 $HOME/myexpos/spl/spl_progs/new/int10.xsm
+load --exhandler $HOME/myexpos/spl/spl_progs/new/exception.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --idle $HOME/myexpos/spl/spl_progs/new/idle.xsm
+load --init $HOME/myexpos/spl/spl_progs/new/init.xsm
+load --exec $HOME/myexpos/spl/spl_progs/new/linked.xsm
