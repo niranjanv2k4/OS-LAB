5e8d8b4a384afcf05ec708954861b30aa8fdc690
Niranjan <niranjanv2k4@gmail.com>
2025-03-26 10:38:15 +0000

    STAGE 27

---
+++ spl/spl_progs/S-27/run.sh
@@ -0,0 +1,69 @@
+load --os $HOME/myexpos/spl/spl_progs/S-27/os-startup.xsm
+load --module 7 $HOME/myexpos/spl/spl_progs/S-27/boot_module.xsm
+load --module 6 $HOME/myexpos/spl/spl_progs/S-27/mod6.xsm
+load --module 5 $HOME/myexpos/spl/spl_progs/S-27/mod5.xsm
+load --module 4 $HOME/myexpos/spl/spl_progs/S-27/mod4.xsm
+load --module 3 $HOME/myexpos/spl/spl_progs/S-27/mod3.xsm
+load --module 2 $HOME/myexpos/spl/spl_progs/S-27/mod2.xsm
+load --module 1 $HOME/myexpos/spl/spl_progs/S-27/mod1.xsm
+load --module 0 $HOME/myexpos/spl/spl_progs/S-27/mod0.xsm
+load --int=timer $HOME/myexpos/spl/spl_progs/S-27/timer.xsm
+load --int=disk $HOME/myexpos/spl/spl_progs/S-27/disk.xsm
+load --int=console $HOME/myexpos/spl/spl_progs/S-27/console.xsm
+load --int=9 $HOME/myexpos/spl/spl_progs/S-27/int9.xsm
+load --int=8 $HOME/myexpos/spl/spl_progs/S-27/int8.xsm
+load --int=7 $HOME/myexpos/spl/spl_progs/S-27/int7.xsm
+load --int=6 $HOME/myexpos/spl/spl_progs/S-27/int6.xsm
+load --int=5 $HOME/myexpos/spl/spl_progs/S-27/int5.xsm
+load --int=4 $HOME/myexpos/spl/spl_progs/S-27/int4.xsm
+load --int=10 $HOME/myexpos/spl/spl_progs/S-27/int10.xsm
+load --int=11 $HOME/myexpos/spl/spl_progs/S-27/int11.xsm
+load --int=12 $HOME/myexpos/spl/spl_progs/S-27/int12.xsm
+load --int=13 $HOME/myexpos/spl/spl_progs/S-27/int13.xsm
+load --int=14 $HOME/myexpos/spl/spl_progs/S-27/int14.xsm
+load --int=15 $HOME/myexpos/spl/spl_progs/S-27/int15.xsm
+load --int=16 $HOME/myexpos/spl/spl_progs/S-27/int16.xsm
+load --int=17 $HOME/myexpos/spl/spl_progs/S-27/int17.xsm
+load --exhandler $HOME/myexpos/spl/spl_progs/S-27/exception.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --idle $HOME/myexpos/spl/spl_progs/S-27/idle.xsm
+load --shell $HOME/myexpos/spl/spl_progs/S-27/shell.xsm
+load --init $HOME/myexpos/spl/spl_progs/S-27/init.xsm
+rm pid.xsm
+rm fork.xsm
+rm create.xsm   
+rm odd.xsm
+rm even.xsm
+rm lu.xsm
+rm ru.xsm
+rm ls.xsm
+rm rm.xsm
+rm cat.xsm
+rm as3.xsm
+rm pgm1.xsm
+rm pgm2.xsm
+rm as2.xsm
+rm as4.xsm
+rm merge.xsm
+rm m_merge.xsm
+rm m_sort.xsm
+rm m_store.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/pid.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/fork.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/create.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/odd.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/even.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/lu.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/ru.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/ls.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/rm.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/as3.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/pgm1.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/cat.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/as2.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/as4.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/pgm2.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/merge.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/m_merge.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/m_sort.xsm
+load --exec $HOME/myexpos/spl/spl_progs/S-27/m_store.xsm
